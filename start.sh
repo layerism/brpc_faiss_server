20e88930ec8e20b55dbdb91529eb42645bf29769
layerism <lcyqky@icloud.com>
2019-10-08 00:52:11 +0800

    remove comment

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 
-#rm -rf third_party && docker container prune
-#nvidia-docker run -itd --name brpc_faiss_server -v `pwd`/../tmp:/data/saved_rocksdb_faiss -v `pwd`/:/opt/brpc_server -p 8330:8330 lyk/brpc_faiss_server:base.v3 /bin/bash
 
 export OMP_NUM_THREADS=1
 mkdir -p /data/saved_rocksdb_faiss
